88298d0c6ade502d7424348f77c4225ac9eed884
Gayan Muthukumara <muthukum_cs17020@stu.kln.ac.lk>
2022-04-15 12:45:40 +0530

    second

---
+++ Core 3.1/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SLE_System.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+        [Required]
+        [Display(Name = "NIC")]
+        public string NIC { get; set; }
+
+        [Required]
+        [Display(Name = "Vehical Registration Number")]
+        public string VehicalRNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Insurance Company Number")]
+        public string InsuranceCompanyName { get; set; }
+
+        [Required]
+        [Display(Name = "Insurance ID Number")]
+        public string InsuranceIDNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+
+
+
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+         
+        [DataType(DataType.Password)]
+        [Display(Name ="Confirm Password")]
+        [Compare("Password",ErrorMessage ="Password and confirmation password not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
